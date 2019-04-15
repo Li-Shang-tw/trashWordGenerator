1b0120497efa086d634bd539280c272650aa2306
Li-Shang-tw <41882910+Li-Shang-tw@users.noreply.github.com>
2019-04-15 13:03:54 +0800

    import personList.json

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const career = require('./personList.json')
 const trashwordGenerate = require('./generate_trashwords.js')
 const helpers = require('handlebars-helpers')();
+
 
 //define server related variable
 const app = express()
@@ -21,13 +23,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //set router
 //首頁
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { career: career.results })
 })
 //取得post的表單資料
 app.post('/', (req, res) => {
   const person = req.body.person
-  const trashwords = trashwordGenerate(person)  
-  res.render('index', { trashwords, person })
+  const trashwords = trashwordGenerate(person)
+  res.render('index', { trashwords, person, career: career.results })
 })
 
 // starts the express server and listening for connections.
